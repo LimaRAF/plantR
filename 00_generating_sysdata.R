6990d75e3fb7a90ba7026399c441b830487e9f74
saramortara <saramortara@gmail.com>
2019-12-12 14:19:10 -0300

    adding code to generate sysdata. Encoding is bad :P

---
+++ 00_generating_sysdata.R
@@ -0,0 +1,38 @@
+# Script to read dictionaries and generate the R/sysdata.rda
+
+# loading packages
+library(stringr)
+
+# testei com todos as opções de ANSI disponível em iconvlist
+all_incov <- iconvlist()
+# nenhum dos ANSI ou WINDOWS funcionam
+ansi <- all_incov[str_detect(all_incov, "ANSI")]
+ruwin <- all_incov[str_detect(all_incov, "WINDOWS")]
+
+dic_files <- list.files(path = "dictionaries",
+                        pattern = "csv",
+                        full.names = TRUE)
+
+encoding <- "UTF-8" # substituir aqui pelo encoding correto
+
+dic <- lapply(dic_files, read_csv, locale = locale(encoding = encoding))
+
+lapply(dic, head)
+
+# dai imagino que usaria o iconv para transformar em UTF-8
+encoding_to <- "UTF-8"
+dic <- lapply(dic, iconv, from = encoding, to = encoding_to)
+
+# aqui to fazendo na mao para manter o nome dos objetos
+autores <- dic[[1]]
+collectionCodes <- dic[[2]]
+families_synonyms <- dic[[3]]
+field_names <- dic[[3]]
+gazetteer <- dic[[4]]
+
+save(autores,
+     collectionCodes,
+     families_synonyms,
+     field_names,
+     gazetteer,
+     file = "R/sysdata.rda")
