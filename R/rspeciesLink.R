e1e45c406062e4c9189ce122a2e8de6ce0eb86fd
saramortara <saramortara@gmail.com>
2020-04-23 12:25:05 -0300

    updating rspeciesLink

--- R/rspeciesLink.R
+++ R/rspeciesLink.R
@@ -15,7 +15,7 @@
 #' @param Coordinates Specify if records should have coordinates. Default is "no check" but it also accepts "Yes", No", "Original", "Automatic", "Blocked" or "no check"
 #' @param CoordinatesQuality Any character in "Good" or "Bad" to select specific type of coordinates
 #' @param Scope Group to be required. If NULL searches all groups. Any in "plants", "animals", "microrganisms" or "fossils"
-# @param Synonyms If species names should be checked for synonyms in a specific dictionary. Set to "species2000" for search in Catálogo da Vida species2000, "flora2020" for Flora do Brasil 2020, "MycoBank" for MycoBank, "AlgaeBase" for AlgaeBase, "DSMZ" for  DSMZ Prokaryotic Nomenclature Up-to-Date, "Moure" for Catálogo de Abelhas Moure or "no synonyms". It does not support more than nine species to check (length of scientificName vector must be < 10)
+#' @param Synonyms If species names should be checked for synonyms in a specific dictionary. Set to "species2000" for search in Catálogo da Vida species2000, "flora2020" for Flora do Brasil 2020, "MycoBank" for MycoBank, "AlgaeBase" for AlgaeBase, "DSMZ" for  DSMZ Prokaryotic Nomenclature Up-to-Date, "Moure" for Catálogo de Abelhas Moure or "no synonyms". It does not support more than nine species to check (length of scientificName vector must be < 10)
 #' @param Typus Logic. If TRUE select only typus
 #' @param Images If select only records with images. Default is NULL. It accepts: "Yes", "Live", "Polen", "Wood"
 #' @param RedList Logic. If TRUE only species in the IUCN Red List are returned
@@ -43,7 +43,7 @@ rspeciesLink <- function(dir = "results/",
                          Coordinates = NULL, #		Yes | No | Original | Automatic | Blocked
                          CoordinatesQuality = NULL,	#Good | Bad
                          Scope = NULL, #			plants, animals, microrganisms,fossils
-                         #Synonyms="no synomyms", #species2000 | flora2020 | MycoBank | AlgaeBase | DSMZ | Moure no synonyms
+                         Synonyms = "no synomyms", #species2000 | flora2020 | MycoBank | AlgaeBase | DSMZ | Moure no synonyms
                          Typus = FALSE,
                          Images = NULL,
                          RedList = FALSE,
@@ -155,21 +155,21 @@ rspeciesLink <- function(dir = "results/",
       my_url <- paste0(my_url, sc)
     }
   }
-  # Synonyms
+  #  Synonyms
   # if (length(scientificName) > 9) {
-  #   stop("Function does not support synonym check of more than nine species")
-  # } else {
-  # if (is.null(Synonyms)) {
-  #   my_url
-  # } else {
-  #   if (Synonyms %in% c("species2000", "flora2020",
-  #                       "MycoBank", "AlgaeBase", "DSMZ")) {
-  #     sy <- url_query(Synonyms, "Synonyms")
-  #     my_url <- paste0(my_url, sy)
-  #   }
-  # }
-  # }
-  # Typus
+  #    stop("Function does not support synonym check of more than nine species")
+  #  } else {
+  if (is.null(Synonyms)) {
+    my_url
+  } else {
+    if (Synonyms %in% c("species2000", "flora2020",
+                        "MycoBank", "AlgaeBase", "DSMZ")) {
+      sy <- url_query(Synonyms, "Synonyms")
+      my_url <- paste0(my_url, sy)
+    }
+  }
+  #  }
+  #  Typus
   if (Typus == FALSE) {
     my_url
   } else {
@@ -210,16 +210,14 @@ rspeciesLink <- function(dir = "results/",
   #rrr <- readr::read_tsv(rr, locale = readr::locale(encoding = "UTF-8"))
   fullname <- paste0(dir, filename, ".csv")
   message(paste0("Writing ", fullname, " on disk."))
-  write.table(
-    rrr,
-    fullname,
-    sep = ",",
-    row.names = FALSE,
-    col.names = TRUE
-  )
+  write.table(rrr,
+              fullname,
+              sep = ",",
+              row.names = FALSE,
+              col.names = TRUE)
   # if output is empty, return message
   if (is.null(dim(rrr))) {
-  message("Output is empty. Check your request.")
+    message("Output is empty. Check your request.")
   }
   return(list(data = rrr, url = my_url))
-  }
+}
