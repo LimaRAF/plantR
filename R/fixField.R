05b06925ef3ef1dc92fe2a60f1e8887388f08160
Andrea Sánchez Tapia <katori@gmail.com>
2020-07-22 22:19:31 -0700

    docs + check

--- R/fixField.R
+++ R/fixField.R
@@ -3,12 +3,11 @@
 #' @description Puts the occurrence data frame in the right format for data processing and validation
 #'
 #' @param x a data frame
-#' @param origin code of collection from which the input data was downloaded
+#' @param origin code of the source from which the input data was downloaded.
+#' Has to be any of thw following: "gbif", "splink", "jabot", "splink2gbif" or "jabot_old"
 #'
 #' @return the data frame \code{x} in the proper format. It also returns the
 #' required field names that are missing and those that were replaced or dropped.
-#'
-#' @details This function ...
 #'
 #' @author Lima, R.A.F.
 #'
@@ -17,13 +16,10 @@
 #'
 #' @export fixField
 #'
-#' @examples
-#'  ## Example currently not available ##
 #'
-#'
-fixField = function(x, origin = NULL) {
+fixField <- function(x, origin = NULL) {
   # check input:
-  if (!class(x) == "data.frame") { stop("input object needs to be a data frame!") }
+  if (!class(x) == "data.frame") stop("input object needs to be a data frame!")
 
   # Getting the fields that are essential for the validation
   fields <- fieldNames
@@ -38,7 +34,7 @@ fixField = function(x, origin = NULL) {
                            fields$standard_name[fields$plantR_status %in% "input_required"][miss.essential],]
     if (is.null(origin)) {
       stop("please provide one of the following origins for your occurrence data:
-           gbif, splink, jabot, splink2gbif or jabot_old")
+           'gbif', 'splink', 'jabot', 'splink2gbif' or 'jabot_old'")
     } else {
       good.names <- good.names[,c("standard_name", origin)]
       problem.names <- df.names[df.names %in% good.names[,origin]]
@@ -66,12 +62,12 @@ fixField = function(x, origin = NULL) {
                                                 input_optional"][miss.optional], ]
     if (is.null(origin)) {
       stop("please provide one of the following origins for your occurrence data:
-           gbif, splink,	jabot, splink2gbif or jabot_old")
+           'gbif', 'splink', 'jabot', 'splink2gbif' or 'jabot_old'")
     } else {
       good.names <- good.names[!is.na(good.names[, origin]), c("standard_name", origin)]
       problem.names <- df.names[df.names %in% good.names[,origin]]
       if (length(problem.names) == 0 ) {
-        comment(x) <- ("No substituion of names for optional fields!")
+        comment(x) <- ("No substitution of names for optional fields!")
       } else  {
         problem.order <- match(df.names, good.names[, origin])
         problem.order <- problem.order[!is.na(problem.order)]
@@ -88,7 +84,7 @@ fixField = function(x, origin = NULL) {
     }
   }
 
-  ## Replacing and removing overlapping fiels
+  ## Replacing and removing overlapping fields
   #replacing  'countryCode' by 'country'
   if (sum(df.names %in% c("county", "municipality")) == 2) {
     code.dig <- nchar(x$countryCode)
