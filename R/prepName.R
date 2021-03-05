d4739ef528cd092c0045dce1f5494f28768fd773
raflima1981 <raflima@usp.br>
2021-03-05 11:04:45 +0100

    Minor changes and code cleaning in function prepName()

--- R/prepName.R
+++ R/prepName.R
@@ -8,8 +8,9 @@
 #'   standardized? Default to TRUE.
 #' @param output a character string with the type of output desired: all names,
 #'   first name, or auxiliary names.
-#' @param treat.prep character
-#' @param ... Parameters from fixName
+#' @param treat.prep a character or vector containing the treatment prepositions
+#' that should be removed from names. Default to some common prpositions in
+#' Portuguese, Spanish and English (see Examples).
 #'
 #' @inherit fixName params
 #' @inherit prepTDWG params
@@ -27,11 +28,11 @@
 #'   should be returned: names of all person ("all", the default), first person's
 #'   names ("first") or all but the first person's names ("aux").
 #'
-#'   The function identifies (and removes) name prefixes or prepositions (e.g. de,
-#'   dos, van, ter, ...). Also, it removes some titles (i.e. Dr., Prof., Pe., Sr., Mr.)
-#'   but not all of them (e.g. Doctor, Priest, Mister, etc.). The function also does not
-#'   handle hyphenated first names. If only one name is given, the function
-#'   returns \code{x} with the first letter capitalized.
+#'   The function identifies (and removes) name prefixes or prepositions (e.g.
+#'   de, dos, van, ter, ...). Also, it removes some titles (i.e. Dr., Dra., Pe.,
+#'   Sr., Mrs.), but not all of them (e.g. Doctor, Priest, Mister, etc.). The
+#'   function also does not handle hyphenated first names. If only one name is
+#'   given, the function returns \code{x} with the first letter capitalized.
 #'
 #'   The function has the option of stardardizing the general notation of names
 #'   and the general format of names. These standardizations are controled by
@@ -77,6 +78,7 @@
 #'
 #'   # Names with titles
 #'   prepName("Pe. Raulino Reitz")
+#'   prepName("Dra. Gloria Galeano")
 #'   prepName("Prof. Hermogenes de Freitas Leitao Filho")
 #'   prepName("Sir G.T. Prance")
 #'   prepName("Sir G.T. Prance", treat.prep = "Sir")
@@ -100,11 +102,11 @@
 #'
 prepName <- function(x,
                      fix.names = TRUE,
+                     output = "all",
+                     treat.prep = c("Dr.", "Dra.", "Pe.", "Prof.", "Profa.", "Sr.", "Sra.", "Mr.", "Mrs.", "Mme."),
                      sep.in = c(";", "&", "|", " e ", " y ", " and ", " und ", " et "),
                      sep.out = "|",
                      special.char = FALSE,
-                     output = "all",
-                     treat.prep = c("Dr.", "Dra.", "Pe.", "Prof.", "Profa.", "Sr.","Sra.", "Mr.", "Mrs.", "Mme.", "Sir", "Dame"),
                      format = "last_init",
                      get.prep = FALSE,
                      get.initials = TRUE) {
@@ -124,16 +126,15 @@ prepName <- function(x,
   # Editing the general name notation
   if (fix.names)
     x <- fixName(x, sep.in = sep.in, sep.out = sep.out, special.char = special.char)
-    #x <- fixName(x, ...)
 
-  # removing treatment prepositions (e.g. Dr., Prof., Pe., ...)
+  # removing treatment prepositions (e.g. Dr., etc)
   patt.treat <-
     gsub("\\.", "\\\\.", paste(treat.prep, collapse = " |"))
   if (!grepl("\\|", patt.treat))
-    patt.treat <- paste0(patt.treat," ")
+    patt.treat <- paste0(patt.treat, " ")
   x <- gsub(patt.treat, "", x, perl = TRUE)
 
-  # Spliting the list of multiple people's names into a list
+  # Splitting the list of multiple people's names into a list
   sep.in1 <- gsub("(\\|)", "\\\\\\1", sep.in, perl = TRUE)
   sep.in1 <- gsub("(\\+)", "\\\\\\1", sep.in1, perl = TRUE)
   sep.in1 <- gsub("(\\$)", "\\\\\\1", sep.in1, perl = TRUE)
@@ -161,54 +162,30 @@ prepName <- function(x,
   DT <- data.table::setDT(data.table::transpose(split, fill = NA))
   dt <- as.data.frame(DT)
   cols <- colnames(dt)
-  # cols <- data.table::copy(names(DT))
   DT[ , tmp.ordem := .I, ]
 
   if (output %in% c("all", "first")) {
-    # data.table::setkeyv(DT, c("V1")) # bm 20s with 3 spp
-    # DT[, V1 := lapply(V1, prepTDWG,
-    #                   format = format, get.prep = get.prep, get.initials = get.initials),
-    #    by = c("V1")]
-    dt$V1 <- prepTDWG(dt$V1, # bm 0.25s with 3 spp
+    dt$V1 <- prepTDWG(dt$V1,
                       format = format, get.prep = get.prep, get.initials = get.initials)
   }
 
   if (output %in% c("all", "aux") & length(cols) > 1) {
-    # cols1 <- cols[!cols %in% c("V1", "tmp.ordem")]
-    # data.table::setkeyv(DT, c(cols1)) # bm 37s with 3 spp
-    # DT[, (cols1) := lapply(.SD, prepTDWG,
-    #                        format = format, get.prep = get.prep, get.initials = get.initials),
-    #    by = cols1, .SDcols = cols1]
     cols1 <- cols[!cols %in% c("V1", "tmp.ordem")]
-    for(i in cols1) # bm 0.25s with 3 spp
+    for(i in cols1)
       dt[, i] <- prepTDWG(dt[, i],
                           format = format, get.prep = get.prep, get.initials = get.initials)
   }
 
   # Preparing to return the result as a vector again
   if (output == "first") {
-    # data.table::setkeyv(DT, "tmp.ordem")
-    # names.out <- as.character(DT$V1)
     names.out <- as.character(dt$V1)
   }
 
   if (output == "aux" & length(cols) == 1) {
-    # data.table::setkeyv(DT, "tmp.ordem")
-    # names.out <- as.character(DT$V1)
     names.out <- rep(NA_character_, dim(dt)[1])
   }
 
   if (output == "aux" & length(cols) > 1) {
-    # DT[, V1:= NULL]
-    # cols1 <- cols[!cols %in% c("V1", "tmp.ordem")]
-    # data.table::setkeyv(DT, c(cols1))
-    # DT[ , names := do.call(paste, c(.SD, sep = sep.out)),
-    #     by = cols1, .SDcols = cols1]
-    # data.table::setkeyv(DT, "names")
-    # DT[ , names := gsub(paste0(sep.out1, "NA"), "", names, perl = TRUE), by = "names"]
-    # DT[ , names := gsub("NA", NA_character_, names, fixed = TRUE), by = "names"]
-    # data.table::setkeyv(DT, "tmp.ordem")
-    # names.out <- DT$names
     cols1 <- cols[!cols %in% c("V1", "tmp.ordem")]
     names <- apply(dt[, cols1], 1, paste, collapse = sep.out)
     names <- gsub(paste0(sep.out1, "NA"), "", names, perl = TRUE)
@@ -218,21 +195,11 @@ prepName <- function(x,
 
   if (output == "all" | !output %in% c("first", "aux")) {
     if (length(cols) > 1) {
-      # cols1 <- cols[!cols %in% c("tmp.ordem")]
-      # data.table::setkeyv(DT, c(cols1))
-      # DT[ , names := do.call(paste, c(.SD, sep = sep.out)),
-      #     by = cols1, .SDcols = cols1]
-      # data.table::setkeyv(DT, "names")
-      # DT[ , names := gsub(paste0(sep.out1, "NA"), "", names, perl = TRUE), by = "names"]
-      # DT[ , names := gsub("NA", NA_character_, names, fixed = TRUE), by = "names"]
-      # data.table::setkeyv(DT, "tmp.ordem")
-      # names.out <- DT$names
       names <- apply(dt[, cols], 1, paste, collapse = sep.out)
       names <- gsub(paste0(sep.out1, "NA"), "", names, perl = TRUE)
       names <- gsub("NA", NA_character_, names, fixed = TRUE)
       names.out <- as.character(names)
     } else {
-      # names.out <- DT$V1
       names.out <- as.character(dt$V1)
     }
   }
