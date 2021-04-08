0022a7ca8b9cbfd6212a0de7dd479d36cab94f0b
raflima1981 <raflima@usp.br>
2021-04-08 23:09:03 +0200

    updating Gitignore, README, tutorial, vignette and adding the bioRxiv preprint

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
+
 <img src="https://raw.githubusercontent.com/liibre/plantR_logo/master/figs/plantR_logo.png?token=AA4OYDE3TMIXBWRYVMNCINC72PIUY" align="right" alt="" width="120" />
 
 # plantR
@@ -8,7 +9,10 @@ An R Package for Managing Species Records from Biological Collections
 
 ### Description
 
-The package plantR provides tools for downloading, processing, cleaning, validating, summarizing and exporting records of plant species occurrences from biological collections. Please read the Introduction and Tutorial of the package for more details.
+The package plantR provides tools for downloading, processing, cleaning,
+validating, summarizing and exporting records of plant species
+occurrences from biological collections. Please read the Introduction
+and Tutorial of the package for more details.
 
 #### Installation
 
@@ -22,22 +26,41 @@ library("plantR")
 
 ##### Bug report and suggestions
 
-The plantR project is hosted on [GitHub](https://github.com/LimaRAF/plantR/). Please, report any bugs and suggestions of improvements for the package [here](https://github.com/LimaRAF/plantR/issues).
+The plantR project is hosted on
+[GitHub](https://github.com/LimaRAF/plantR/). Please, report any bugs
+and suggestions of improvements for the package
+[here](https://github.com/LimaRAF/plantR/issues).
 
-The package gazetteer and the list of taxonomists are constantly being improved. If you want to contribute with regional gazetteers or with missing names of taxonomists, please e-mail to <raflima@usp.br>.
+The package gazetteer and the list of taxonomists are constantly being
+improved. If you want to contribute with regional gazetteers or with
+missing names of taxonomists, please e-mail to <raflima@usp.br>.
 
 ### Authors and contributors
 
-Renato A. F. de Lima, Sara R. Mortara, Andrea Sánchez-Tapia, Hans ter Steege & Marinez F. de Siqueira
+Renato A. F. de Lima, Sara R. Mortara, Andrea Sánchez-Tapia, Hans ter
+Steege & Marinez F. de Siqueira
 
 ### Citation
 
-Lima, R.A.F et al. (2020) *plantR*: An R package and workflow for managing species records from biological collections. In preparation.
+Lima, R.A.F., Sánchez-Tapia, A., Mortara, S.R., ter Steege, H.,
+Siqueira, M.F. (2021). *plantR*: An R package and workflow for managing
+species records from biological collections. bioRxiv: 2021.04.06.437754.
+<https://doi.org/10.1101/2021.04.06.437754>
 
 ### Funding
 
-The development of this package was supported by the European Union’s Horizon 2020 research and innovation program under the Marie Skłodowska-Curie grant agreement No 795114.
+The development of this package was supported by the European Union’s
+Horizon 2020 research and innovation program under the Marie
+Skłodowska-Curie grant agreement No 795114, by the Coordination for the
+Improvement of Higher Education Personnel (CAPES, process
+88887.145924/2017-00), and by the ‘Instituto Nacional da Mata Atlântica’
+(INMA).
 
 ### Acknowledgements
 
-We thank Sidnei Souza from speciesLink for his help with the web API. We also thank the [CNCFlora](http://cncflora.jbrj.gov.br) and the [TreeCo database](http://labtrop.ib.usp.br/doku.php?id=projetos:treeco:start) for providing many of the localities used to construct the package gazetteer. We also thank Vinícius C. Souza (ESALQ/USP) who helped to validate and improve the list of plant taxonomists used in the package.
+We thank Sidnei Souza from speciesLink for his help with the web API. We
+also thank the [CNCFlora](http://cncflora.jbrj.gov.br) and the [TreeCo
+database](http://labtrop.ib.usp.br/doku.php?id=projetos:treeco:start)
+for providing many of the localities used to construct the package
+gazetteer. We also thank Vinícius C. Souza (ESALQ/USP) who helped to
+validate and improve the list of plant taxonomists used in the package.
--- vignettes/articles/tutorial.R
+++ vignettes/articles/tutorial.R
@@ -1,32 +1,74 @@
 ## Tutorial
-spp <- c("Trema micrantha",
-         "Casearia sylvestris",
-         "Euterpe edulis")
-#example <- rgbif2(species =  spp)
-example <- rspeciesLink(species =  spp,
-                        Scope = "plants",
-                        basisOfRecord = "PreservedSpecimen",
-                        Synonyms = "flora2020")
-occs <- fixField(example$data, origin = "splink")
-occs <- formatOcc(occs)
 
-##Add a function/step to format collectionCodes
-saveRDS(occs, "tmp.rds")
-occs <- readRDS("tmp.rds")
-occs <- fixLoc(occs, admin.levels = c("country","stateProvince","municipality","locality"), scrap = TRUE)
+# Data entry
+spp <- c("Casearia sylvestris",
+         "Euterpe edulis",
+         "Trema micrantha")
+occs_splink <- rspeciesLink(species =  spp,
+                            Scope = "plants",
+                            basisOfRecord = "PreservedSpecimen",
+                            Synonyms = "flora2020")
+occs_gbif <- rgbif2(species =  spp,
+                    basisOfRecord = "PRESERVED_SPECIMEN",
+                    remove_na = FALSE, limit = 500000)
+nrow(occs_splink)
+nrow(occs_gbif)
+occs <- formatDwc(splink_data = occs_splink,
+                  gbif_data = occs_gbif, drop = TRUE)
+dim(occs)
+
+# Data editing
+occs <- getCode(occs)
+occs$recordedBy.new <- prepName(occs$recordedBy,
+                                output = "first",
+                                sep.out = "; ")
+occs$recordedBy.aux <- prepName(occs$recordedBy,
+                                output = "aux",
+                                sep.out = "; ")
+occs$identifiedBy.new <- prepName(occs$identifiedBy,
+                                  output = "first",
+                                  sep.out = "; ")
+occs$identifiedBy.aux <- prepName(occs$identifiedBy,
+                                  output = "aux",
+                                  sep.out = "; ")
+occs$recordedBy.new <- missName(occs$recordedBy.new,
+                                type = "collector", noName = "s.n.")
+occs$identifiedBy.new <- missName(occs$identifiedBy.new,
+                                  type = "identificator", noName = "s.n.")
+occs$last.name <- lastName(occs$recordedBy.new)
+occs$recordNumber.new <- colNumber(occs$recordNumber, noNumb = "s.n.")
+occs$year.new <- getYear(occs$year, noYear = "n.d.")
+occs$yearIdentified.new <- getYear(occs$dateIdentified, noYear = "n.d.")
+
+
+occs <- fixLoc(occs,
+               loc.levels = c("country","stateProvince","municipality","locality"),
+               scrap = TRUE)
 locs <- strLoc(occs)
-locs$loc.string <- prepLoc(locs$loc.string) # priority string
+locs$loc.string  <- prepLoc(locs$loc.string) # priority string
 locs$loc.string1 <- prepLoc(locs$loc.string1) # alternative string
 locs$loc.string2 <- prepLoc(locs$loc.string2) # alternative string
 locs <- getLoc(locs, gazet = "plantR")
-occs <- cbind.data.frame(occs,  locs[,c("loc","loc.correct","latitude.gazetteer","longitude.gazetteer","resolution.gazetteer")])
+occs <- cbind.data.frame(occs, locs[, c("loc","loc.correct","latitude.gazetteer","longitude.gazetteer","resolution.gazetteer")])
+
 occs <- prepCoord(occs)
 occs <- getCoord(occs)
-occs <- validateLoc(occs)
-occs <- prepSpecies(occs)
-occs <- formatSpecies(occs, db = c("bfo", "tpl"), sug.dist = 0.85)
+
+occs <- fixSpecies(occs)
+occs <- prepSpecies(occs, db = c("bfo", "tpl"), sug.dist = 0.85)
 occs$scientificName.new <- occs$suggestedName
-occs <- formatFamily(occs)
+occs <- prepFamily(occs)
+
+occs <- validateLoc(occs)
+unique(occs$loc.check)
+
+occs <- checkCoord(occs,
+                   keep.cols = c("geo.check", "NAME_0", "country.gazet"))
+unique(occs$geo.check)
+
+
+
+
 occs <- validateTax(occs)
 dups <- prepDup(occs,
                 comb.fields = list(c("family","col.last.name","col.number","municipality"),
@@ -46,13 +88,14 @@ occs <- getCult(occs)
 
 
 ##Vinheta
+devtools::load_all()
 spp <- c("Trema micrantha",
          "Casearia sylvestris",
          "Euterpe edulis")
 occs_splink1 <- rspeciesLink(species = spp)
 occs_gbif1 <- rgbif2(species = spp)
-occs_splink <- rspeciesLink(species = "Euterpe edulis")
-occs_gbif <- rgbif2(species = "Euterpe edulis")
+# occs_splink <- rspeciesLink(species = "Euterpe edulis")
+# occs_gbif <- rgbif2(species = "Euterpe edulis")
 occs.bind <- formatDwc(splink_data = occs_splink1,
                   gbif_data = occs_gbif1)
 occs0 <- formatOcc(occs.bind)
