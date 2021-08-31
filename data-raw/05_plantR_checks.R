eaf86a5f5afe569f37548102e77d7bb252716ab1
raflima1981 <raflima@usp.br>
2021-08-31 17:02:34 +0200

    plantR checks, examples and good practices codes

---
+++ data-raw/05_plantR_checks.R
@@ -0,0 +1,43 @@
+#### CHECKING, TESTING AND SPELLING ####
+
+#Checking
+devtools::run_examples()
+devtools::check()
+
+#Testing
+#ROpenSci: Test coverage below 75% will likely require additional tests or
+#explanation before being sent for review.
+devtools::test()
+
+#Spelling
+# spelling::update_wordlist(pkg = ".", vignettes = FALSE, confirm = TRUE)
+# spelling::get_wordlist(pkg = ".")
+toto <- spelling::spell_check_package(vignettes = TRUE)
+toto1 <- spelling::spell_check_package(vignettes = TRUE, lang = "pt_BR")
+
+#Checking by word
+toto1 <- lengths(toto$found)
+names(toto1) <- toto$word
+head(tail(sort(toto1), 40))
+
+palavras <- c("coden","Curitiba’","numb'and"
+)
+toto2 <- toto$found[match(palavras, toto$word)]
+names(toto2) <- palavras
+toto2
+
+#Checking by file
+# tail(sort(table(unlist(toto$found))), 10)
+# spelling::spell_check_files("man/validateCoord.Rd")
+spelling::spell_check_files("./vignettes/plantr_introduction.Rmd",
+                                     lang = "en_US")
+spelling::spell_check_files("./vignettes/articles/plantr_tutorial.Rmd",
+                                     lang = "en_US")
+spelling::spell_check_files("./vignettes/articles/atualiza_duplicatas.Rmd",
+                            lang = "pt_BR")
+
+#Good practices
+goodpractice::gp()
+goodpractice::gp(".", checks = c("rcmdcheck_tests_pass"))
+
+
