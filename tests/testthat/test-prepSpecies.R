1ac139c16196ee7fa13af2dca4ab7a6c1265b2d2
raflima1981 <raflima@usp.br>
2022-12-30 14:49:35 +0100

    Fixing problems related to issues #90 and #96 (The Plant List website was discontinued)

--- tests/testthat/test-prepSpecies.R
+++ tests/testthat/test-prepSpecies.R
@@ -11,13 +11,15 @@ df <- data.frame(scientificName.new = sp.list,
 res0 <- c("Casearia sylvestris Sw.", "Casearia sylvestris Sw.", NA,
           "Casearia sylvestris Sw.", "Casearia ulmifolia Vahl ex Vent.",
           "Casearia sylvestris Sw.", "Casearia tropicana")
+# res1 <- c("Casearia sylvestris Sw.", "Casearia sylvestris Sw.", NA,
+#           "Casearia sylvestris Sw.", "Casearia ulmifolia Vahl ex Vent.",
+#           "Casearia ulmifolia Vahl ex Vent.", "Casearia tropicana")
 res1 <- c("Casearia sylvestris Sw.", "Casearia sylvestris Sw.", NA,
-          "Casearia sylvestris Sw.", "Casearia ulmifolia Vahl ex Vent.",
-          "Casearia ulmifolia Vahl ex Vent.", "Casearia tropicana")
+          "Casearia sylvestris Sw.", NA, NA, NA)
 
 # Tests
 test_that("prepSpecies works", {
-  expect_equal(prepSpecies(df)$scientificNameFull, res0)
-  expect_equal(prepSpecies(df[, 1, drop = FALSE])$scientificNameFull, res1)
+  expect_equal(prepSpecies(df)$scientificNameFull, res1)
+  expect_equal(prepSpecies(df[, 1, drop = FALSE], use.authors = FALSE)$scientificNameFull, res1)
   expect_equal(prepSpecies(df, use.authors = FALSE)$scientificNameFull, res1)
 })
