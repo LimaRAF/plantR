45572419b008ed0bb94738dfb6e1fa8c1d255913
Renato Lima <raflima@usp.br>
2025-04-11 09:32:21 -0300

    Adding or improving some function tests

--- tests/testthat/test-buildName.R
+++ tests/testthat/test-buildName.R
@@ -3,4 +3,12 @@ test_that("buildName works", {
   df <- data.frame(genus = c("Aa", "Ab", "Ac"),
                    specificEpithet = c("aa", "bb", NA))
   expect_equal(buildName(df), c("Aa aa","Ab bb", "Ac"))
+
+  expect_error(buildName(c("Aa aa","Ab bb", "Ac")))
+
+  df <- data.frame(jojo = c("Aa", "Ab", "Ac"),
+                   toto = c("aa", "bb", NA))
+  expect_error(buildName(df))
+
+
 })
