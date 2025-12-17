fa8174b38e89736ec1a1d4f83342ecf883816761
Renato Lima <raflima@usp.br>
2025-12-17 16:19:39 +0100

    Improving the management of collection codes

--- tests/testthat/test-getCode.R
+++ tests/testthat/test-getCode.R
@@ -1,11 +1,14 @@
-df <- data.frame(institutionCode = c("ASU", "UNEMAT", "MOBOT", "NYBG", NA,
+# df <- data.frame(institutionCode = c("ASU", "UNEMAT", "MOBOT", "NYBG"),
+# collectionCode = c("ASU-PLANTS", "NX-FANEROGAMAS", "MO", "NY"))
+df <- data.frame(institutionCode = c("ASU", "UNEMAT", "MOBOT", "NYBG", "AUA", "HAST", NA,
                                      "Observation",
                                      NA, NA, "", NA, ""),
-                 collectionCode = c("ASU-PLANTS", "NX-FANEROGAMAS", "MO", "NY", "MO",
+                 collectionCode = c("ASU-PLANTS", "NX-FANEROGAMAS", "MO", "NY", "", "HAST-100012", "MO",
                                     NA,
                                     "Xuxu", NA, NA, "", ""))
-res <- c("ASU", "NX", "MO", "NY", "MO", "Observation", "Xuxu", NA, NA, NA, NA)
-res1 <- c(rep(NA, 5), rep("code not found", 6))
+
+res <- c("ASU", "NX", "MO", "NY", "AUA", "HAST","MO", "Observation", "Xuxu", NA, NA, NA, NA)
+res1 <- c(rep(NA, 6), "code partially found", rep("code not found", 2), rep("cannot check", 4))
 
 test_that("getCode works", {
   expect_error(getCode())
@@ -37,4 +40,18 @@ test_that("getCode parameter works", {
   expect_true(all(setdiff(names(df_code), names(df)) %in%
                     c("collectionCode.new", "collectionObs", "organization")))
   expect_equal(nrow(df_code), nrow(df))
+})
+
+df <- data.frame(institutionCode = c(NA, NA, "",
+                                    "HERB", "Botany", "NA", ## false NAs that are actually the code NA from United States National Arboretum
+                                    "NY", "NY", "HERB", "CCM", "NO DISPONIBLE", "NA", NA, ""),
+                 collectionCode = c("RB", "AA", "AA",
+                                    "NA", "NA", "NA",  ## false NAs that are actually the code NA from United States National Arboretum
+                                    NA, "", "-", "NO DISPONIBLE", "NO DISPONIBLE", "unknown", NA, ""))
+res2 <- c(rep("code partially found", 5), rep(NA, 3),
+          rep("code not found", 4), rep("cannot check", 2))
+
+test_that("getCode parameter works", {
+  df_code <- getCode(df)
+  expect_equal(df_code$collectionObs, res2)
 })
